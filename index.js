41e07259e2d32c315e37ed6ffc5e954a673298d3
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-02-06 11:27:20 -0200

    Update to requirejs-memfiles 2.1.15-3

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
-var mutex, queue, requireCssFiles,
+var requireCssFiles,
 	fs = require( "fs" ),
 	glob = require( "glob" ),
 	path = require( "path" ),
 	requirejs = require( "requirejs-memfiles" ),
 	util = require( "util" );
 
-queue = [];
 requireCssFiles = {};
 
 glob.sync( __dirname + "/bower_components/require-css/**", { nodir: true } ).forEach(function( filepath ) {
 	requireCssFiles[ filepath.replace( __dirname, "" ).replace( /^\//, "" ) ] = fs.readFileSync( filepath );
 });
 
-function enqueueBuildCss() {
-	queue.push( arguments );
-	if ( queue.length === 1 ) {
-		dequeueBuildCss();
-	}
-}
-
-function dequeueBuildCss() {
-	var callback;
-	var args = queue[ 0 ];
-	if ( args !== undefined ) {
-		callback = args[ 2 ];
-		args[ 2 ] = function() {
-			callback.apply( {}, arguments );
-			queue.shift();
-			dequeueBuildCss();
-		};
-		buildCss.apply( {}, args );
-	}
-}
-
 function buildCss( files, config, callback ) {
-	var localCallback, include;
+	var include;
 
-	if ( mutex ) {
-		return callback( new Error( "Concurrent calls not supported" ) );
-	}
 	if ( typeof config !== "object" ) {
 		return callback( new Error( "missing or invalid config (object expected)" ) );
 	}
 	if ( !Array.isArray( config.include ) ) {
 		return callback( new Error( "missing or invalid config.include (array expected)" ) );
 	}
-	mutex = true;
-	localCallback = function( error, css ) {
-		mutex = false;
-		callback( error, css, files );
-	};
 
 	include = config.include;
 	delete config.include;
@@ -92,10 +62,15 @@ function buildCss( files, config, callback ) {
 		}]
 	});
 
-	requirejs.setFiles( files );
-	requirejs.optimize( config, function() {
-		localCallback( null, files[ "dist/output.css" ] );
-	}, localCallback );
+	requirejs.setFiles( files, function( done ) {
+		requirejs.optimize( config, function() {
+			callback( null, files[ "dist/output.css" ], files );
+			done();
+		}, function( error ) {
+			callback( error );
+			done();
+		});
+	});
 }
 
 /**
@@ -113,5 +88,5 @@ module.exports = function( files, config, callback ) {
 		}
 	});
 
-	enqueueBuildCss( clonedFiles, config, callback );
+	buildCss( clonedFiles, config, callback );
 };
